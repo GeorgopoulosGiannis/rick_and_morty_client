c00f6ee0b74a4208def3c0b169ca319dc87d0b1f
GeorgopoulosIoannis <64830751+GeorgopoulosGiannis@users.noreply.github.com>
2021-11-06 19:57:21 +0200

    Add widget test

---
+++ test/src/features/characters_list/presentation/widgets/list_card_test.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:rick_and_morty_client/src/core/domain/entities/character.dart';
+import 'package:rick_and_morty_client/src/core/domain/entities/character_status.dart';
+import 'package:rick_and_morty_client/src/core/domain/entities/location.dart';
+import 'package:rick_and_morty_client/src/core/domain/entities/origin.dart';
+import 'package:rick_and_morty_client/src/features/characters_list/presentation/widgets/list_card.dart';
+
+void main() {
+  const char = Character(
+    id: 1,
+    name: 'tSame',
+    status: CharacterStatus.alive,
+    species: 'tSpecies',
+    type: 'tType',
+    gender: 'tGender',
+    origin: Origin('tOriginName', 'tOriginLocation'),
+    location: Location(name: 'tLocationName', lastKnown: 'tLastknown'),
+    imageUrl: 'tImageUrl',
+    episode: [],
+    url: 'turl',
+  );
+
+  group(
+    'List Card',
+    () {
+      testWidgets('Should display image', (WidgetTester tester) async {
+        await tester.pumpWidget(
+          MaterialApp(
+            builder: (context, widget) {
+              return Material(
+                child: ListCard(
+                  char: char,
+                  onTap: () {},
+                ),
+              );
+            },
+          ),
+        );
+        final imageFinder = find.byType(Image);
+        expect(imageFinder, findsOneWidget);
+      });
+    },
+  );
+}
